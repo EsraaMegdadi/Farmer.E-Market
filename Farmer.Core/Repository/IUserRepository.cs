0f36c9c8cee574ce898cd9f685b08b0d91a8d952
Dalia Mahmoud <daliaSayaheen@outlook.com>
2021-08-06 22:26:43 +0300

    edit

---
+++ Farmer.Core/Repository/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farmer.Core.Repository
+{
+   public interface IUserRepository
+    {
+        List<Users> GetAll();
+       int Create(Users Data);
+        int Update(Users Data);
+
+        int Delete(int id);
+    }
+}
