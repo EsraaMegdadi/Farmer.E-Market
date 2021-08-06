a79782a3b08762eff9400bfc85a00265fa4aa6ed
dohamutlaq123@gmail.com <dohamutlaq123@gmail.com>
2021-08-06 23:29:52 +0300

    add class cat+proc+rev >>

---
+++ Farmer.Core/Data/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Farmer.Core.Data
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+
+        [Display(Name = "CategoryName")]
+        [Required(ErrorMessage = "Enter CategoryName")]
+        public string CategoryName { get; set; }
+
+
+        public ICollection<Products> products { get; set; }
+    }
+}
