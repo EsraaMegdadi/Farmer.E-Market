c4418838e580945247b2563314fe42b85d7cfc5a
Dalia Mahmoud <daliaSayaheen@outlook.com>
2021-09-11 16:42:08 +0300

    testJoin

---
+++ Farmer.Core/DTO/ProductsCategoryDTO.cs
@@ -0,0 +1,19 @@
+﻿using Farmer.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farmer.Core.DTO
+{
+   public class ProductsCategoryDTO
+    {
+        
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductImg { get; set; }
+        public ICollection<Products> products { get; set; }
+
+    }
+}
